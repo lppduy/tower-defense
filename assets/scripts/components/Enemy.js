7d69870af9dbdd77c1e5566a9102cfb1a3735f5e
lppduy <lephamphuongduy@gmail.com>
2023-12-12 21:09:58 +0700

    turning the enemy

--- assets/scripts/components/Enemy.js
+++ assets/scripts/components/Enemy.js
@@ -9,6 +9,7 @@ cc.Class({
       default: null,
       type: LevelMap,
     },
+    _rotationSpeed: 300,
   },
 
   start() {
@@ -28,6 +29,7 @@ cc.Class({
       this.node.emit('finished');
       this.node.destroy();
     } else {
+      this.rotateTo(targetPosition);
       this.moveTo(targetPosition).then(() => {
         this.targetIndex++;
         this.move();
@@ -35,13 +37,29 @@ cc.Class({
     }
   },
 
-  moveTo(targetPosition) {
-    return new Promise(resolve => {
-      const x = Math.abs(targetPosition.x - this.node.x);
-      const y = Math.abs(targetPosition.y - this.node.y);
-      const distance = Math.max(x, y);
+  getAngle(targetPosition) {
+    const result =
+      (Math.atan2(targetPosition.y - this.node.y, targetPosition.x - this.node.x) * -180) / Math.PI;
+    return result;
+  },
 
-      const time = distance / this.velocity;
+  rotateTo(targetPosition) {
+    const angle = this.getAngle(targetPosition);
+    let distance = Math.abs(angle - this.node.angle);
+
+    if (distance) {
+      const time = distance / this._rotationSpeed;
+      this.node.runAction(cc.rotateTo(time, angle));
+    }
+  },
+
+  moveTo(targetPosition) {
+    const x = Math.abs(targetPosition.x - this.node.x);
+    const y = Math.abs(targetPosition.y - this.node.y);
+    const distance = Math.max(x, y);
+    const time = distance / this.velocity;
+
+    return new Promise(resolve => {
       const moveToAction = cc.moveTo(time, targetPosition);
       const sequence = cc.sequence(moveToAction, cc.callFunc(resolve));
       this.node.runAction(sequence);

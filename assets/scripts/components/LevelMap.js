6324127c742ebcac2a68042a48f3ff18f660aa34
lppduy <lephamphuongduy@gmail.com>
2023-12-12 14:44:49 +0700

    added map api

--- assets/scripts/components/LevelMap.js
+++ assets/scripts/components/LevelMap.js
@@ -3,11 +3,28 @@ cc.Class({
 
   properties: {},
 
-  // LIFE-CYCLE CALLBACKS:
+  start() {
+    this.tiledMap = this.getComponent(cc.TiledMap);
 
-  // onLoad () {},
+    this.roadsLayer = this.tiledMap.getLayer('roads');
+    this.towersLayer = this.tiledMap.getLayer('towers');
 
-  start() {},
+    this.tiledSize = this.tiledMap.getTileSize();
+    this.tileWidth = this.tiledSize.width;
+    this.tileHeight = this.tiledSize.height;
 
-  // update (dt) {},
+    this.mapSize = this.tiledMap.getMapSize();
+    this.mapWidth = this.mapSize.width;
+    this.mapHeight = this.mapSize.height;
+
+    const coords = this.getTileCoordinatesByPosition(cc.v2(100, 100));
+    cc.log(coords);
+  },
+
+  getTileCoordinatesByPosition(position) {
+    return {
+      x: Math.round(position.x / this.tileWidth),
+      y: this.mapHeight - Math.round(position.y / this.tileHeight) - 1,
+    };
+  },
 });

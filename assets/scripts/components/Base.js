941d0a35a64a5167a9090a5c1a159cfb25fddc1a
user <user@Eno-ITs-MacBook-Pro.local>
2023-12-20 18:19:31 +0700

    Add vase explosion animation

--- assets/scripts/components/Base.js
+++ assets/scripts/components/Base.js
@@ -1,13 +1,18 @@
 const MainEmitter = require('MainEmitter');
 const { GAME_EVENTS } = require('EventCode');
-
+const Emitter = require('EventEmitter');
+const Key = require('Key');
 cc.Class({
   extends: cc.Component,
 
   properties: {
     healthBar: cc.ProgressBar,
-    maxHealth:50,
-    curHealth:0,
+    maxHealth: 50,
+    curHealth: 0,
+    explosionSound: {
+      default: null,
+      type: cc.AudioClip,
+    },
   },
 
   onLoad() {
@@ -18,6 +23,9 @@ cc.Class({
     this.curHealth -= damage;
     this.healthBar.progress = this.curHealth / this.maxHealth;
     if (this.curHealth <= 0) {
+      Emitter.instance.emit(Key.PLAY_SFX, this.explosionSound);
+      this.node.getChildByName('Base Explosion').active = true;
+      this.healthBar.active = false;
       MainEmitter.instance.emit(GAME_EVENTS.END_GAME);
       this.node.getChildByName('Sprite').color = cc.Color.GRAY;
     }
